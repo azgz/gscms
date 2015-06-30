7265295893228399c0b63f5f74cee288d857ef3c
azgz <azmax.koichi+gz@gmail.com>
2015-06-30 14:02:50 +0900

    sql取得・表示処理を実装

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 // SQLを実行し、DBから一覧を取得
+// TODO エラー時の処理
+$pdo = new PDO('mysql:dbname=gs_db;host=localhost', 'root', '');
+$stmt = $pdo->query('SET NAMES utf8');
+$stmt = $pdo->prepare("SELECT * FROM gs_cms_user");
+$stmt->execute();
+$view = "";
 // SQLの結果から、HTMLを生成
+while ($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $id = $result['id'];
+    $name = $result['name'];
+    $email = $result['email'];
+    $age = $result['age'];
+    $create_date = $result['create_date'];
+    $view .= '<ul><li>'. $id . '</li><li>'. $name . '</li><li>' . $email . '</li><li>'
+    		 . $age . '</li><li>' . $create_date . '</ul>';
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -18,6 +33,7 @@
 		<li>登録時間</li>
 	</ul>
 	<!-- データベースの取得結果はここで表示 -->
+	<?php echo $view ?>
 </div>
 <div>
 	<ul>
