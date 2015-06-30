4335f4fd29d035d89ca7565bbc43661bad150eff
azgz <azmax.koichi+gz@gmail.com>
2015-06-30 13:47:00 +0900

    Topページhtmlをコミット

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>g's CMS | Top</title>
+</head>
+<body>
+<div>
+	<ul>
+		<li>id</li>
+		<li>名前</li>
+		<li>e-mail</li>
+		<li>年齢</li>
+		<li>登録時間</li>
+	</ul>
+</div>
+<div>
+	<ul>
+		<li><a href="register.php">登録</a></li>
+	</ul>
+</div>
+</body>
+</html>
