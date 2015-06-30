a00f0cbc7216c4cd343ec4314105522bc3855601
azgz <azmax.koichi+gz@gmail.com>
2015-06-30 17:35:26 +0900

    form先のurlの変更とコメント欄の追加

--- register.php
+++ register.php
@@ -5,10 +5,11 @@
   <title>g's CMS | ユーザー登録</title>
 </head>
 <body>
-<form method="post" action="regist_execute.php">
+<form method="post" action="register_execute.php">
 <p>お名前:<input type="text" name="name" size="20"></p>
 <p>e-mail:<input type="text" name="mail" size="20"></p>
 <p>年齢:<input type="text" name="age" size="20"></p>
+<p>コメント:<input type="text" name="info" size="20"></p>
 <p><input type="submit" value="送信"></p>
 </form>
 
