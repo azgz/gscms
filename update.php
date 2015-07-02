8a4135c74994abc0f23596c7b152a3328a83a2cf
azgz <azmax.koichi+gz@gmail.com>
2015-07-02 09:48:38 +0900

    更新処理を実装

--- update.php
+++ update.php
@@ -1,6 +1,23 @@
 <?php
 // index.phpからidが渡ってくるので、取得
+// TODO idが渡ってこない場合はエラーにして、index.phpへ戻す
+$id = $_GET["id"];
+
 // SQLを実行し、DBから該当idのデータを取得
+// TODO エラー時の処理
+$pdo = new PDO('mysql:dbname=gs_db;host=localhost', 'root', '');
+$stmt = $pdo->query('SET NAMES utf8');
+$stmt = $pdo->prepare("SELECT * FROM gs_cms_user WHERE id = :id");
+$stmt->bindParam(':id', $id);
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// SQLの結果から、HTML出力用の変数を定義
+$name = $result['name'];
+$email = $result['email'];
+$age = $result['age'];
+$info = $result['info'];
+
 // 取得したデータをformに埋め込む
 ?>
 <!DOCTYPE html>
@@ -11,11 +28,12 @@
 </head>
 <body>
 <form method="post" action="update_execute.php">
-<p>お名前:<input type="text" name="name" size="20"></p>
-<p>e-mail:<input type="text" name="mail" size="20"></p>
-<p>年齢:<input type="text" name="age" size="20"></p>
-<p>コメント:<input type="text" name="info" size="20"></p>
-<p><input type="submit" value="送信"></p>
+<p>お名前:<input type="text" name="name" size="20" value="<?php echo $name ?>"></p>
+<p>e-mail:<input type="text" name="mail" size="20" value="<?php echo $email ?>"></p>
+<p>年齢:<input type="text" name="age" size="20" value="<?php echo $age ?>"></p>
+<p>コメント:<input type="text" name="info" size="20" value="<?php echo $info ?>"></p>
+<p><input type="submit" value="更新"></p>
+<input type="hidden" name="id" value="<?php echo $id ?>">
 </form>
 
 <div>
--- update_execute.php
+++ update_execute.php
@@ -1,5 +1,29 @@
 <?php
 // formから値を取得
+$id = $_POST["id"];
+$name = $_POST["name"];
+$mail = $_POST["mail"];
+$age  = $_POST["age"];
+$info  = $_POST["info"];
+
+// TODO 入力チェックを実装
+
 // SQLを実行し、データをアップデート
+// TODO エラー時の処理
+$pdo = new PDO('mysql:dbname=gs_db;host=localhost', 'root', '');
+$stmt = $pdo->query('SET NAMES utf8');
+$stmt = $pdo->prepare("UPDATE gs_cms_user SET
+		name = :name, email = :email, age = :age, info = :info, update_date = sysdate()
+		WHERE id = :id");
+
+$stmt->bindValue(':id', $id);
+$stmt->bindValue(':name', $name);
+$stmt->bindValue(':email', $mail);
+$stmt->bindValue(':age', $age);
+$stmt->bindValue(':info', $info);
+$flag = $stmt->execute();
+
 // update_complete.php へリダイレクト
+header('location: update_complete.php');
+exit;
 ?>
