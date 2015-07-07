b7e80467b167751a5f716ce1fa871b2e5354c2da
azgz <azmax.koichi+gz@gmail.com>
2015-07-07 09:48:38 +0900

    削除処理を追加

--- delete_execute.php
+++ delete_execute.php
@@ -1,5 +1,18 @@
 <?php
 // idを取得
+$id = $_POST["id"];
+
 // SQLを実行し、データをデリート
+// TODO エラー時の処理
+$pdo = new PDO('mysql:dbname=gs_db;host=localhost', 'root', '');
+$stmt = $pdo->query('SET NAMES utf8');
+$stmt = $pdo->prepare("DELETE FROM gs_cms_user
+		WHERE id = :id");
+
+$stmt->bindValue(':id', $id);
+$result = $stmt->execute();
+
 // delete_complete.php へリダイレクト
+header('location: delete_complete.php');
+exit;
 ?>
