8ec15c34cd5f3a31a68a38ed8c4da1958a791df3
azgz <azmax.koichi+gz@gmail.com>
2015-07-08 10:47:47 +0900

    管理者ログイン情報を、データベースと照合する処理を追加

--- admin/login_execute.php
+++ admin/login_execute.php
@@ -1,7 +1,28 @@
 <?php
 // formから値を取得
-// SQLを実行し、e-mail, パスワードが一致するかチェック
+$mail      = $_POST["mail"];
+$password  = $_POST["password"];
+
+// SQLを実行し、e-mail, パスワードが一致するレコードがあるかチェック
+$pdo = new PDO('mysql:dbname=gs_db;host=localhost', 'root', '');
+$stmt = $pdo->query('SET NAMES utf8');
+$stmt = $pdo->prepare("SELECT * FROM gs_cms_admin_user
+		WHERE email = :email AND password = :password");
+
+$stmt->bindValue(':email', $mail);
+$stmt->bindValue(':password', $password);
+$stmt->execute();
+$count = $stmt->rowCount();
+
 // 失敗時はlogin.php へリダイレクト
-// 成功時はセッションにログイン情報をセット
+if($count != 1) {
+	header('location: login.php');
+	exit;
+}
+
+// TODO 成功時はセッションにログイン情報をセット
+
 // index.php へリダイレクト
+header('location: index.php');
+exit;
 ?>
